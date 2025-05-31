3ea27e9cfcad1172cf615f34585d63902358a6ec
anbari1337 <amine.a@wiggli.io>
2025-05-31 12:27:33 +0100

    fix: remove @DirtiesContext annotation use @Transactional instead

--- src/test/java/com/imedia24/productWatcher/integration/AbstractIntegrationTest.java
+++ src/test/java/com/imedia24/productWatcher/integration/AbstractIntegrationTest.java
@@ -7,13 +7,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 @EnableConfigurationProperties
 @ActiveProfiles("test")
